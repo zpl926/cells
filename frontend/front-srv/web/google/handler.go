6014c321bb0b31b48bed6c2b22a8b13c4a5ac593
zpl <zfl926@163.com>
2024-11-01 15:46:18 +0800

    remove token

--- frontend/front-srv/web/google/handler.go
+++ frontend/front-srv/web/google/handler.go
@@ -30,8 +30,6 @@ import (
 
 var (
 	googleOAuthConfig = &oauth2.Config{
-		// ClientID:     "722972143299-5kj5fqfs1agvoploinoggv2tcbd32cuj.apps.googleusercontent.com",
-		// ClientSecret: "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",
 		RedirectURL: "https://frs.forthtech.io/google/auth/callback",
 		Scopes:      []string{"openid", "profile", "email"},
 		Endpoint:    google.Endpoint,
